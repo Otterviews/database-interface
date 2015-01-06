c5f935342abd6c2ee4f01c9a26b6a2008c6b2d7e
Pedro Viegas <pedroasv@gmail.com>
2015-01-06 20:25:55 +0000

    updating akka dependency

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
             :dependencies [[org.clojure/clojure "1.6.0"]
                            [clojurewerkz/titanium "1.0.0-beta1"]
                            [org.clojure.pasviegas/okku "1.4"]
-                           [com.typesafe.akka/akka-actor "2.0"]
+                           [com.typesafe.akka/akka-actor_2.11 "2.3.8"]
                            [cheshire "5.4.0"]]
             :repositories [["typesafe" {:url  "http://repo.typesafe.com/typesafe/releases/"
                                         :id   "typesafe"
